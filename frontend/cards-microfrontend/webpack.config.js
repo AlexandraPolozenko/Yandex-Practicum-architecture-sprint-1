3f12da7fab0db4a8ef9902823328c01fc297a7e2
AlexandraPolozenko <34188316+AlexandraPolozenko@users.noreply.github.com>
2024-11-07 13:03:05 +0100

    Update webpack.config.js

--- frontend/cards-microfrontend/webpack.config.js
+++ frontend/cards-microfrontend/webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     name: "cards",
     filename: "cardsIndex.js",
     exposes: {
-        
+        './Card': './src/components/Card',
+        './AddPlacePopup': './src/components/AddPlacePopup',
     },
     shared: ["react"]
     };
